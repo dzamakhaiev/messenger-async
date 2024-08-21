101f6b0adaaa0f2d715b4518910a9f3b9db61e2c
dzamakhaiev <dzamakhaiev@star.global>
2024-08-21 16:16:14 +0300

    add create user func

--- app/users.py
+++ app/users.py
@@ -1,16 +1,57 @@
 import sys
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Request, HTTPException, BackgroundTasks, status
+from pydantic import ValidationError
 from uvicorn import run
 from app import routes
 from app import settings
+from models.users_models import User
+from services.db_service import DBService
+from logger.logger import Logger
 
 
+users_logger = Logger('users_endpoint')
 app = FastAPI()
+db_service = DBService()
+
+
+async def establish_db_connection():
+    users_logger.debug('Connecting to databases.')
+    await db_service.connect_to_databases()
+    await db_service.prepare_databases_data()
+    users_logger.debug('Connection is established.')
+
+
+async def close_db_async_connection():
+    users_logger.debug('Closing async connections with databases.')
+    await db_service.close_all_connections()
+    users_logger.debug('Connections are closed.')
 
 
 @app.post(path=routes.USERS, status_code=status.HTTP_201_CREATED)
-async def create_user():
-    return {}
+async def create_user(request: Request, background_tasks: BackgroundTasks):
+    users_logger.info('Create user.')
+    await establish_db_connection()
+    background_tasks.add_task(close_db_async_connection)
+
+    try:
+        request_json = await request.json()
+        users_logger.debug(f'Request body: {request_json}')
+        user = User(**request_json)
+
+    except ValidationError as e:
+        users_logger.error(e)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=settings.VALIDATION_ERROR)
+
+    users_logger.debug(f'Check username in database: {user.username}')
+    db_user = await db_service.get_user(username=user.username)
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=settings.USER_EXISTS_ERROR)
+
+    user_id = await db_service.create_user(user)
+    users_logger.info(f'User "{user.username}" created with id {user_id}')
+    return {'user_id': user_id}
 
 
 @app.get(path=routes.USERS, status_code=status.HTTP_200_OK)
