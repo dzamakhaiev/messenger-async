89988a2e57f1c2a487c91be91a6961955d0506bc
dzamakhaiev <dzamakhaiev@star.global>
2024-08-23 19:16:48 +0300

    add container names

--- databases/settings.py
+++ databases/settings.py
@@ -1,6 +1,9 @@
 import os
 INSIDE_DOCKER = int(os.environ.get('RUN_INSIDE_DOCKER', 0))
 
+MQ_CONTAINER_NAME = 'rabbitmq'
+PG_CONTAINER_NAME = 'postgres'
+
 DB_HOST = 'postgres' if INSIDE_DOCKER else 'localhost'
 DB_PORT = 5432
 DB_NAME = 'postgres'
