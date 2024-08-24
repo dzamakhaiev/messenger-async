2c69ac6f25fced72d1ee3279c76933520f558307
dzamakhaiev <dzamakhaiev@star.global>
2024-08-24 16:36:27 +0300

    remove sqlite from db service

--- services/db_service_old.py
+++ services/db_service_old.py
@@ -1,11 +1,12 @@
 """
 This module contains class Srvice that provide logical layer
-between listener, sender services and database handler.
+between listener, sender services and databases handlers.
 """
 import asyncio
 from typing import List
 from models.users_models import User, UserDB
 from databases.postgres_handler import PostgresHandler
+from databases.sqlite_handler import SQLiteHandler
 from logger.logger import Logger
 
 
@@ -18,15 +19,18 @@ class DBService:
 
     def __init__(self):
         self.hdd_db_handler = PostgresHandler()
+        self.ram_db_handler = SQLiteHandler()
         service_logger.info('DB service is initialized.')
 
     async def connect_to_databases(self):
         if not self.prepared:
+            await self.ram_db_handler.connect()
             await self.hdd_db_handler.connect()
             self.prepared = True
             service_logger.info('Connections to databases are established.')
 
     async def prepare_databases_data(self):
+        await self.ram_db_handler.create_all_tables()
         await self.hdd_db_handler.create_all_tables()
         service_logger.info('Databases are prepared to work.')
 
@@ -82,8 +86,9 @@ class DBService:
         user_record = await self.hdd_db_handler.get_user(username=user.username)
         service_logger.debug(f'User from DB: {user_record}')
         user_db = self.convert_record_to_user(user_record)
-        service_logger.info('New user is created.')
 
+        await self.ram_db_handler.insert_username(user_db.id, user_db.username)
+        service_logger.info('New user is created.')
         return user_db.id
 
     async def store_message_to_db(self, msg_json: dict):
@@ -99,48 +104,72 @@ class DBService:
         service_logger.info('Store user user addresses in HDD and RAM DBs.')
         service_logger.debug(f'User user: {user_address}')
         await self.hdd_db_handler.insert_address(user_address)
+        await self.ram_db_handler.insert_user_address(user_id, user_address)
         await self.hdd_db_handler.insert_user_address(user_id, user_address)
 
     async def store_user_token(self, user_id, token):
         service_logger.info('Store user token.')
         service_logger.debug(token)
         await self.hdd_db_handler.insert_user_token(user_id, token)
+        await self.ram_db_handler.insert_user_token(user_id, token)
 
     async def store_user_public_key(self, user_id, public_key):
         service_logger.info('Store user public key.')
         service_logger.debug(public_key)
         await self.hdd_db_handler.insert_user_public_key(user_id, public_key)
+        await self.ram_db_handler.insert_user_public_key(user_id, public_key)
 
     async def get_user(self, user_id: int = None, username: str = None) -> UserDB | None:
         service_logger.info(f'Get user by "{user_id}" or "{username}".')
         if user_id:
-            user_record = await self.hdd_db_handler.get_user(user_id=user_id)
 
-            if user_record:
-                service_logger.debug(f'User found in HDD by user_id "{user_id}".')
-                user_db = self.convert_record_to_user(user_record)
+            # Check RAM DB first with user_id
+            user_tuple = await self.ram_db_handler.get_user(user_id=user_id)
+            if user_tuple:
+                service_logger.debug(f'User found in RAM by user_id "{user_id}".')
+                user_db = self.convert_tuple_to_user(user_tuple)
                 return user_db
+
+            else:
+                # Check HDD DB with user_id
+                user_record = await self.hdd_db_handler.get_user(user_id=user_id)
+                if user_record:
+                    service_logger.debug(f'User found in HDD by user_id "{user_id}".')
+                    user_db = self.convert_record_to_user(user_record)
+                    return user_db
 
             # If user not found
             service_logger.error(f'User not found by user_id "{user_id}".')
             return None
 
         elif username:
-
-            user_record = await self.hdd_db_handler.get_user(username=username)
-            if user_record:
-                service_logger.debug(f'User found in HDD by username "{username}".')
-                user_db = self.convert_record_to_user(user_record)
+            # Check RAM DB first with username
+            user_tuple = await self.ram_db_handler.get_user(username=username)
+            if user_tuple:
+                service_logger.debug(f'User found in RAM by username "{username}".')
+                user_db = self.convert_tuple_to_user(user_tuple)
                 return user_db
 
-        # If user not found
-        service_logger.error(f'User not found by username "{username}".')
-        return None
+            else:
+                # Check HDD DB with username
+                user_record = await self.hdd_db_handler.get_user(username=username)
+                if user_record:
+                    service_logger.debug(f'User found in HDD by username "{username}".')
+                    user_db = self.convert_record_to_user(user_record)
+                    return user_db
+
+            # If user not found
+            service_logger.error(f'User not found by username "{username}".')
+            return None
 
     async def get_user_address(self, user_id: int) -> List:
         service_logger.info(f'Get user address for user id "{user_id}".')
-        address_list = await self.hdd_db_handler.get_user_address(user_id)
-        service_logger.debug(f'Address list from HDD database: {address_list}')
+        address_list = await self.ram_db_handler.get_user_address(user_id)
+        service_logger.debug(f'Address list from RAM database: {address_list}')
+
+        if not address_list:
+            address_list = await self.hdd_db_handler.get_user_address(user_id)
+            service_logger.debug(f'Address list from HDD database: {address_list}')
 
         if address_list:
             address_list = self.convert_address_list(address_list)
@@ -149,17 +178,25 @@ class DBService:
 
     async def get_user_token(self, user_id: int):
         service_logger.info(f'Get user token for user id "{user_id}".')
-        token = await self.hdd_db_handler.get_user_token(user_id)
+        token = await self.ram_db_handler.get_user_token(user_id)
+
+        if not token:
+            token = await self.hdd_db_handler.get_user_token(user_id)
+
         service_logger.debug(f'User token: "{token}"')
         return token
 
     async def get_user_public_key(self, user_id: int):
         service_logger.info(f'Get public key for user_id "{user_id}".')
-        public_key = await self.hdd_db_handler.get_user_public_key(user_id)
+
+        public_key = await self.ram_db_handler.get_user_public_key(user_id)
+        if public_key is None:
+            public_key = await self.hdd_db_handler.get_user_public_key(user_id)
 
         if public_key:
             service_logger.debug('User public key found.')
             public_key = self.convert_public_key(public_key)
+            await self.ram_db_handler.insert_user_public_key(user_id, public_key)
             return public_key
 
         else:
@@ -174,7 +211,12 @@ class DBService:
 
     async def check_user_token(self, user_id: int, token: str):
         service_logger.info(f'Check user token for user_id "{user_id}".')
-        exp_token = await self.hdd_db_handler.get_user_token(user_id)
+        exp_token = await self.ram_db_handler.get_user_token(user_id)
+
+        if exp_token is None:
+            exp_token = await self.hdd_db_handler.get_user_token(user_id)
+            if exp_token:
+                await self.ram_db_handler.insert_user_token(user_id, token)
 
         if exp_token:
             exp_token = self.convert_token(exp_token)
@@ -192,8 +234,11 @@ class DBService:
         if user:
             await self.delete_user_token(user_id)
             await self.delete_user_public_key(user_id)
+
             await self.hdd_db_handler.delete_user_messages(user_id)
+            await self.ram_db_handler.delete_user_address(user_id)
             await self.hdd_db_handler.delete_user_address(user_id)
+            await self.ram_db_handler.delete_user(user_id=user_id)
             await self.hdd_db_handler.delete_user(user_id=user_id)
 
             service_logger.info(f'User "{user_id}" deleted.')
@@ -205,12 +250,15 @@ class DBService:
     async def delete_user_token(self, user_id: int):
         service_logger.info(f'Check user token for user_id "{user_id}".')
         await self.hdd_db_handler.delete_user_token(user_id)
+        await self.ram_db_handler.delete_user_token(user_id)
 
     async def delete_user_public_key(self, user_id: int):
         service_logger.info(f'Check user public key for user_id "{user_id}".')
         await self.hdd_db_handler.delete_user_public_key(user_id)
+        await self.ram_db_handler.delete_user_public_key(user_id)
 
     async def close_all_connections(self):
+        await self.ram_db_handler.close()
         await self.hdd_db_handler.close()
 
 
